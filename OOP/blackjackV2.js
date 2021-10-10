b70c702a6d9ad0049c8d57a566db6f753fd6f602
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-10-10 16:25:31 -0400

    sum function, prompt to play another round

--- OOP/blackjackV2.js
+++ OOP/blackjackV2.js
@@ -16,7 +16,8 @@ class Player {
     this.hand = hand;
   }
   // sum cards
-  sum() {
+  cardSum() {
+    // convert faces to numbers
     for (let i = 0; i < this.hand.length; i++) {
       if (this.hand[i].value === "Ace") {
         this.hand[i].value = "11";
@@ -28,15 +29,13 @@ class Player {
         this.hand[i].value = "10";
       }
     }
-    // for (let i = 0; i < this.hand.length; i++) {
-    //   this.hand.value[i] = Number(this.hand.value[i]);
-    //   console.log(this.hand.value[i]);
-    // }
-    // const reducer = (previousValue, currentValue) =>
-    //   previousValue + currentValue;
-    // return this.hand.reduce(reducer);
-    let sumHand = Number(this.hand[0].value) + Number(this.hand[1].value);
-    return sumHand;
+    // for loop to convert "5" to 5 and sum each number in array
+    let sum = 0;
+    for (let i = 0; i < this.hand.length; i++) {
+      this.hand[i].value = Number(this.hand[i].value);
+      sum += this.hand[i].value;
+    }
+    return sum;
   }
 }
 
@@ -109,33 +108,34 @@ class Deck {
 
 // create deck
 let deck1 = new Deck();
-
 // create players
 let player1 = new Player("Mickey", [], 100);
 let house = new Player("The House", [], 1000);
-
 // variable to start and end the game
 let gameIsOver = false;
-// FUNCTION TO DETERMINE WINNER
+
+// FUNCTIONS
+
+// Determine winner
 const winner = () => {
   if (
-    player1.sum() === 21 ||
-    (player1.sum() <= 21 && player1.sum() > house.sum())
+    player1.cardSum() === 21 ||
+    (player1.cardSum() <= 21 && player1.cardSum() > house.cardSum())
   ) {
     player1.bankroll += 10;
     console.log(
-      `You won! Your total is ${player1.sum()} and The House' total is ${house.sum()}. Your bankroll is now $${
+      `You won! Your total is ${player1.cardSum()} and The House' total is ${house.cardSum()}. Your bankroll is now $${
         player1.bankroll
       }.`
     );
   } else if (
-    player1.sum() > 21 ||
-    house.sum() === 21 ||
-    (house.sum() > player1.sum() && house.sum() < 21)
+    player1.cardSum() > 21 ||
+    house.cardSum() === 21 ||
+    (house.cardSum() > player1.cardSum() && house.cardSum() < 21)
   ) {
     player1.bankroll -= 10;
     console.log(
-      `You lost! The house total is ${house.sum()} and The House' total is ${player1.sum()}.Your bankroll is now $${
+      `You lost! The House total is ${house.cardSum()} and your total is ${player1.cardSum()}. Your bankroll is now $${
         player1.bankroll
       }.`
     );
@@ -143,25 +143,33 @@ const winner = () => {
   gameIsOver = true;
 };
 
+// Prompt to play another round after game ends
+const anotherGamePrompt = () => {
+  let gamePrompt = prompt("Would you like to play another round? Y/n ");
+  if(gamePrompt === "Y"){
+    gameLoop();
+  } else {
+    gameIsOver = true;
+  }
+}
+
 ///////////////
 // GAME LOOP
 //////////////
-while (!gameIsOver) {
-  // prompt user for name
-  const playerName = prompt("What is your name?");
-  player1.name = playerName;
-  console.log(`Hey there, ${playerName}. Let's play a round!`);
 
-  // start game, deal cards
+const gameLoop = () => {
+  // clear hands, deal two new cards, start game
+  player1.hand = [];
+  house.hand = [];
   player1.hand.push(deck1.deal(), deck1.deal());
   house.hand.push(deck1.deal(), deck1.deal());
   console.log("Player 1: ", player1.hand);
   console.log("The House: ", house.hand);
 
   // prompt for deal or quit
-  let dealPrompt = prompt("Would you like to (d)eal or (q)uit?");
+  let dealPrompt = prompt("Would you like to (d)eal or (q)uit? ");
 
-  // IF PLAYER CHOOSES TO DEAL CARD
+  // IF PLAYER CHOOSES TO DEAL CARD,
   if (dealPrompt === "d") {
     // deal player another card
     player1.hand.push(deck1.deal());
@@ -169,9 +177,9 @@ while (!gameIsOver) {
     console.log("The House: ", house.hand);
 
     // If player hand is less than 21, prompt them to deal or quit, else determine winner and quit
-    if (player1.sum() < 21 && house.sum() != 21) {
+    if (player1.cardSum() < 21 && house.cardSum() != 21) {
       let dealPrompt2 = prompt(
-        "Would you like to (d)eal another card or (q)uit?"
+        "Would you like to (d)eal another card or (q)uit? "
       );
 
       // IF PLAYER CHOOSES TO DEAL ANOTHER CARD
@@ -181,19 +189,36 @@ while (!gameIsOver) {
         console.log("The House: ", house.hand);
         // DETERMINE WINNER
         winner();
+        // Prompt for another round
+        anotherGamePrompt();
 
         // ELSE IF PLAYER CHOOSES TO QUIT AFTER dealPrompt2
       } else {
         winner();
+        anotherGamePrompt();
       }
 
       // CARDS ARE OVER 21, OR HOUSE HAS 21
     } else {
       winner();
+      anotherGamePrompt();
     }
 
     // IF PLAYER CHOOSES TO QUIT AFTER FIRST DEAL PROMPT
   } else {
     winner();
+    anotherGamePrompt();
   }
+};
+
+//////////////////////
+// RUN GAME
+//////////////////////
+while (!gameIsOver) {
+  // prompt user for name
+  const playerName = prompt("What is your name? ");
+  player1.name = playerName;
+  console.log(`Hey there, ${playerName}. Let's play a round!`);
+
+  gameLoop();
 }
